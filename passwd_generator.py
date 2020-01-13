c5105eba439d58d6e76a480f6412e84a542bff65
austin-bell <austinbell.93@gmail.com>
2020-01-12 18:03:16 -0700

    add loop to generate more than one password

--- passwd_generator.py
+++ passwd_generator.py
@@ -5,12 +5,12 @@ chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*'
 
 length = input()
 length = int(length)
-
-# varriable to store password
-password = ''
-for c in range(length):
-  # Add character to specified by number in range
-  password += random.choice(chars)
-
-# print characters from chars
-print(password)
+for p in range(3):
+  # variable to store password
+  password = ''
+  for c in range(length):
+    # Add character to specified by number in range
+    password += random.choice(chars)
+  
+  # print characters from chars
+  print(password)
