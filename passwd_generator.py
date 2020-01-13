21ffc5c46bd24e66691bb7442556ac8de975888e
austin-bell <austinbell.93@gmail.com>
2020-01-12 17:35:54 -0700

    Inital commit

---
+++ passwd_generator.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+import random
+
+chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*'
+
+length = input()
+length = int(length)
+
+# varriable to store password
+password = ''
+for c in range(length):
+  # Add character to specified by number in range
+  password += random.choice(chars)
+
+# print characters from chars
+print(password)
